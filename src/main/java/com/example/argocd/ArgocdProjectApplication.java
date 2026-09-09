985d67154c9649098380047d5ecaa0435681905f
casebass3-max <casebass3@gmail.com>
2026-09-09 15:30:44 +0900

    fix: update ci workflow and dockerhub secrets

--- src/main/java/com/example/argocd/ArgocdProjectApplication.java
+++ src/main/java/com/example/argocd/ArgocdProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArgocdProjectApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ArgocdProjectApplication.class, args);
 	}
 }
